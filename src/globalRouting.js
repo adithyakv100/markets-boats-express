6d5d5a1996d135f7cd85da7e622969341b01ed65
Adithya <32417423+adithyakv100@users.noreply.github.com>
2020-03-07 13:46:19 +0530

    Update globalRouting.js

--- src/globalRouting.js
+++ src/globalRouting.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-//const userRouter = require("./apps/User/user.routes");
+const userRouter = require("./apps/User/user.routes");
 
-//router.use("/auth", userRouter);
+router.use("/auth", userRouter);
 
-router.get("/", (req, res) => {
-  res.send("abcd");
-});
+// router.get("/", (req, res) => {
+//   res.send("abcd");
+// });
 
 module.exports = {
   router
