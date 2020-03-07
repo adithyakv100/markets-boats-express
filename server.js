a31fbb935a1797fdd3b1824ca137f4aa3ef89d6d
Adithya <32417423+adithyakv100@users.noreply.github.com>
2020-03-07 11:57:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
-var express = require("express");
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
 var port = process.env.PORT || 3000;
-var app = express();
+
+const app = express();
+console.log("created an express app");
+
+//app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get("/", function(req, res) {
   res.send({
     Output: "Welcome to Markets-Boats. The server is up and running!"
   });
 });
+
 // app.post("/", function(req, res) {
 //   res.send({
 //     Output: "Welcome to Markets-Boats. The server is up and running!"
 //   });
 // });
 //app.listen(port);
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
 });
