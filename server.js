1ab99d3f052e626ec605f9f6aacdcdc58450ba5c
Adithya <32417423+adithyakv100@users.noreply.github.com>
2020-03-07 11:32:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const { app } = require("./loaders");
-
-const port = 3000;
+var express = require("express");
+var port = process.env.PORT || 3000;
+var app = express();
+app.get("/", function(req, res) {
+  res.send({
+    Output: "Welcome to Markets-Boats. The server is up and running!"
+  });
+});
+// app.post("/", function(req, res) {
+//   res.send({
+//     Output: "Welcome to Markets-Boats. The server is up and running!"
+//   });
+// });
+//app.listen(port);
 app.listen(port, () => {
   console.log(`Server started on port ${port}!`);
 });
-
 module.exports = app;
